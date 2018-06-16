c0751c0efbdc901c8e11b10387feb31005df8176
Norbert Muller <mullernorbert93@gmail.com>
2018-06-16 11:40:55 +0200

    first C# practice

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace firstC_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int[] numbers = {5, 3, 7, 8, 1};
+        int[] sum = new int [1];
+
+        for (int i = 0; i < numbers.Length; i++){
+          sum[0] += numbers[i];
+        }
+        System.Console.Write(sum[0]);
+        }
+    }
+}
